8686876de3e782134578c1afd86b172f580e8bda
BongSujin <rirty123@naver.com>
2022-09-21 11:12:05 +0900

    0921_intent

--- app/src/main/java/com/example/pharmacy_ex1/Join.java
+++ app/src/main/java/com/example/pharmacy_ex1/Join.java
@@ -1,11 +1,38 @@
 package com.example.pharmacy_ex1;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Join extends MainActivity{
+
+    EditText edittextId2, edittextPw2;
+    Button btnJoin2;
+    View dialogView;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.join);
+
+        edittextId2 = (EditText) findViewById(R.id.edId2);
+        edittextPw2 = (EditText) findViewById(R.id.edPw2);
+
+        btnJoin2 = (Button) findViewById(R.id.btnStart);
+
+        btnJoin2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialogView = (View) View.inflate(Join.this, R.layout.joindialog, null);
+                AlertDialog.Builder dlg = new AlertDialog.Builder(Join.this);
+                dlg.setView(dialogView);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/pharmacy_ex1/MainActivity.java
+++ app/src/main/java/com/example/pharmacy_ex1/MainActivity.java
@@ -2,15 +2,35 @@ package com.example.pharmacy_ex1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText edittextId1, edittextPw1;
+    Button btnJoin1, btnLogin1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        edittextId1 = (EditText) findViewById(R.id.edId1);
+        edittextPw1 = (EditText) findViewById(R.id.edPw1);
 
+        btnJoin1 = (Button) findViewById(R.id.btnJoin);
+        btnLogin1 = (Button) findViewById(R.id.btnLogin);
+
+        btnJoin1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Join.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
