2437813d58527ce483feed0ad53f6d821aab743b
youkwa <42166340+youkwa@users.noreply.github.com>
2022-09-21 18:37:48 +0900

    안녕하세요유과입니다

--- app/src/main/java/com/example/pharmacy_ex1/JoinDialog.java
+++ app/src/main/java/com/example/pharmacy_ex1/JoinDialog.java
@@ -10,5 +10,6 @@ public class JoinDialog extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.joindialog);
+//        ㅇㅏㄴ녕하세요꼬추
     }
 }
