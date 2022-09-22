d6ba0a283f16f9e0bf55dd05b3c7b8ca48a70674
BongSujin <rirty123@naver.com>
2022-09-23 00:56:11 +0900

    알람생성

--- app/src/main/java/com/example/pharmacy_ex1/Home.java
+++ app/src/main/java/com/example/pharmacy_ex1/Home.java
@@ -1,14 +1,30 @@
 package com.example.pharmacy_ex1;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Home extends Activity {
+
+    Button alarmStart;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
+
+        alarmStart = (Button) findViewById(R.id.alarm1);
+
+        alarmStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Home.this, Alarm.class);
+                startActivity(intent);
+            }
+        });
     }
 
-//    어라라..
+
 }
