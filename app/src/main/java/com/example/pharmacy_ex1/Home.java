9196fca3c7972551d3f800d3155484a27b8f48aa
HeoJi-Hyun <108704496+HeoJi-Hyun@users.noreply.github.com>
2022-09-23 00:31:48 +0900

    Merge remote-tracking branch 'origin/master'

--- app/src/main/java/com/example/pharmacy_ex1/Home.java
+++ app/src/main/java/com/example/pharmacy_ex1/Home.java
@@ -1,12 +1,47 @@
 package com.example.pharmacy_ex1;
 
 import android.app.Activity;
+import android.app.FragmentManager;
 import android.os.Bundle;
 
-public class Home extends Activity {
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class Home extends Activity implements OnMapReadyCallback {
+    private GoogleMap mMap;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
+        SupportMapFragment mapFragment = (SupportMapFragment)
+                getSupportFragmentManager().findFragmentById(R.id.mapView6);
+        mapFragment.getMapAsync(this);
+
+    }
+
+
+    @Override
+    public void onMapReady(final GoogleMap googleMap) {
+        mMap = googleMap;
+
+        LatLng SEOUL = new LatLng(37.56, 126.97);
+
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(SEOUL);
+        markerOptions.title("서울");
+        markerOptions.snippet("한국의 수도");
+        mMap.addMarker(markerOptions);
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(SEOUL));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+        googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
     }
 }
