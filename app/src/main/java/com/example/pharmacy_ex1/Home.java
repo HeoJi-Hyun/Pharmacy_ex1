7392b33ab10977aa4c35dd5f2d06d3ddf055285f
HeoJi-Hyun <108704496+HeoJi-Hyun@users.noreply.github.com>
2022-09-23 19:26:51 +0900

    메인화면에서 바로가기 두개 추가(나중에 지울거임)

--- app/src/main/java/com/example/pharmacy_ex1/Home.java
+++ app/src/main/java/com/example/pharmacy_ex1/Home.java
@@ -1,13 +1,64 @@
 package com.example.pharmacy_ex1;
 
 import android.app.Activity;
+
+import android.app.FragmentManager;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
 
-public class Home extends Activity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class Home extends Activity  implements OnMapReadyCallback{
+    private FragmentManager fragmentManager;
+    private MapFragment mapFragment;
+
+//    SettingFragment settingFragment;
+
+//    Button alarmStart;
+
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.home);
+
+        fragmentManager = getFragmentManager();
+        mapFragment = (MapFragment)fragmentManager.findFragmentById(R.id.googleMap);
+        mapFragment.getMapAsync(this);
+
+//
+//        alarmStart = (Button) findViewById(R.id.alarm1);
+//
+//        alarmStart.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(Home.this, Alarm.class);
+//                startActivity(intent);
+//            }
+//        });
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng location = new LatLng(37.485284,126.901451);
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.title("구로디지털단지역");
+        markerOptions.snippet("전철역");
+        markerOptions.position(location);
+        googleMap.addMarker(markerOptions);
+
+
+
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location,16));
+
+
     }
 }
--- app/src/main/java/com/example/pharmacy_ex1/MainActivity.java
+++ app/src/main/java/com/example/pharmacy_ex1/MainActivity.java
@@ -12,7 +12,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     EditText edittextId1, edittextPw1;
-    Button btnJoin1, btnLogin1;
+    Button btnJoin1, btnLogin1,btn8, btn9;
     Integer v1, v2;
 
     @Override
@@ -23,6 +23,11 @@ public class MainActivity extends AppCompatActivity {
         edittextId1 = (EditText) findViewById(R.id.edId1);
         edittextPw1 = (EditText) findViewById(R.id.edPw1);
         btnLogin1 = (Button) findViewById(R.id.btnLogin);
+        btn8 = (Button)findViewById(R.id.btn8);
+        btn9 = (Button)findViewById(R.id.btn9) ;
+
+
+
 
         btnJoin1 = (Button) findViewById(R.id.btnJoin);
 
@@ -45,13 +50,27 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 if(edittextId1.getText().toString().equals(v1.toString())&&edittextPw1.getText().toString().equals(v2.toString())){
 
-                    Intent intent1 = new Intent(MainActivity.this, Map.class);
+                    Intent intent1 = new Intent(MainActivity.this, Home.class);
 
                     startActivity(intent1);
                 }
                 else{
                     Toast.makeText(getApplicationContext(),"아이디 혹은 비밀번호를 확인하세요",Toast.LENGTH_SHORT).show();
                 }
+            }
+        });
+        btn8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent8 = new Intent(MainActivity.this, Alarm.class);
+                startActivity(intent8);
+            }
+        });
+        btn9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent9 = new Intent(MainActivity.this, Home.class);
+                startActivity(intent9);
             }
         });
     }
