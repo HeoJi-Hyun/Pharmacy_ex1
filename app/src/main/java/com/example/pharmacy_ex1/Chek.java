c8579393953d201bbe8a6fa9f11bd42783d573c5
HeoJi-Hyun <108704496+HeoJi-Hyun@users.noreply.github.com>
2022-10-03 16:16:09 +0900

    다들 업데이트해주세요

--- app/src/main/java/com/example/pharmacy_ex1/Chek.java
+++ app/src/main/java/com/example/pharmacy_ex1/Chek.java
@@ -1,21 +1,56 @@
+
 package com.example.pharmacy_ex1;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.SearchView;
+import android.widget.ToggleButton;
 
 import androidx.annotation.Nullable;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 
 
 public class Chek extends Activity {
 
-    SearchView searchView;
+    EditText editText1;
+    ToggleButton togle1, togle2, togle3;
+    Button btnOk;
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.chek);
 
-//
+
+        btnOk = (Button)findViewById(R.id.ChekOk);
+        editText1 = (EditText)findViewById(R.id.edit1);
+        togle1 = (ToggleButton)findViewById(R.id.tog1);
+        togle2 = (ToggleButton)findViewById(R.id.tog2);
+
+        btnOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (editText1.getText() == togle1.getTextOff())
+                    togle1.setTextColor(Color.BLUE);
+                else if(editText1.getText() == togle1.getTextOn())
+                    togle1.setTextColor(Color.BLUE);
+                else if(editText1.getText() == togle1.getText())
+                    togle1.setTextColor(Color.BLUE);
+                else {
+                    togle1.setTextColor(Color.RED);
+                }
+            }
+        });
+
     }
 
 }
--- app/src/main/java/com/example/pharmacy_ex1/Prov.java
+++ app/src/main/java/com/example/pharmacy_ex1/Prov.java
@@ -11,18 +11,57 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.material.tabs.TabLayout;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class Prov extends Activity {
+public class Prov extends AppCompatActivity {
 
+
+    Frag1 frag1;
+    Frag2 frag2;
+    Frag3 frag3;
+
+    TabLayout tabs;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.prov);
 
+        frag1 = new Frag1();
+        frag2 = new Frag2();
+        frag3 = new Frag3();
+
+        getSupportFragmentManager().beginTransaction().add(R.id.container, frag1).commit();
+
+        tabs = findViewById(R.id.tabs);
+        tabs.addTab(tabs.newTab().setText("성분/효능"));
+        tabs.addTab(tabs.newTab().setText("용법"));
+        tabs.addTab(tabs.newTab().setText("주의사항"));
+        tabs.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                int position = tab.getPosition();
+                Fragment selected = null;
+                if(position == 0)
+                    selected = frag1;
+                else if(position == 1)
+                    selected = frag2;
+                else if(position == 2)
+                    selected = frag3;
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, selected).commit();
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
 
     }
-    private FragmentManager getSupportFragmentManager(){
-        return null;
-    }
+
 }
