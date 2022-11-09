d77440cf059bebbfa1401077afca106d2fd9c146
HeoJi-Hyun <108704496+HeoJi-Hyun@users.noreply.github.com>
2022-11-09 21:08:02 +0900

    카메라 실행됩니당

--- app/src/main/java/com/example/pharmacy_ex1/Home.java
+++ app/src/main/java/com/example/pharmacy_ex1/Home.java
@@ -3,7 +3,6 @@ package com.example.pharmacy_ex1;
 import android.app.Activity;
 
 import android.content.Intent;
-import android.graphics.Camera;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -79,7 +78,7 @@ public class Home extends Activity  implements OnMapReadyCallback{
         btnMy.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent8 = new Intent(Home.this, Mypage.class);
+                Intent intent8 = new Intent(Home.this, Camera.class);
 
                 startActivity(intent8);
             }
