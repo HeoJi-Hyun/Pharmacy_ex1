7e1aaf55ef4436d9cc695c116f8e04a5f63b1dd6
youkwa <42166340+youkwa@users.noreply.github.com>
2022-11-25 18:07:59 +0900

    가니 Home2 지도- 현재위치+주변 타이틀까지 완료

---
+++ app/src/main/java/com/example/pharmacy_ex1/Home2.java
@@ -0,0 +1,50 @@
+package com.example.pharmacy_ex1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class Home2 extends AppCompatActivity
+        implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home2);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(final GoogleMap googleMap) {
+
+        mMap = googleMap;
+
+        LatLng DEFAULT_LOCATION =new LatLng(37.5888101627338, 127.06563586510103);
+
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(DEFAULT_LOCATION);
+        markerOptions.title("삼육보건대학교");
+        markerOptions.snippet("삼육보건대학교");
+        mMap.addMarker(markerOptions);
+
+        // 기존에 사용하던 다음 2줄은 문제가 있습니다.
+        // CameraUpdateFactory.zoomTo가 오동작하네요.
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(SEOUL));
+        //mMap.animateCamera(CameraUpdateFactory.zoomTo(10));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(DEFAULT_LOCATION, 18));
+
+    }
+
+}
+
