f0f144f689203a7fac171cb53680c5d814e2cf6d
BongSujin <rirty123@naver.com>
2022-12-10 19:19:56 +0900

    고장난 알람시계

--- app/src/main/java/com/example/pharmacy_ex1/AlertReceiver.java
+++ app/src/main/java/com/example/pharmacy_ex1/AlertReceiver.java
@@ -7,10 +7,12 @@ import android.content.Intent;
 import androidx.core.app.NotificationCompat;
 
 public class AlertReceiver extends BroadcastReceiver {
+    String message;
+
     @Override
     public void onReceive(Context context, Intent intent) {
-        NotificationHelper notificationHelper = new NotificationHelper(context);
-        NotificationCompat.Builder nb = notificationHelper.getChannelNotification();
+        NotificationHelper notificationHelper = new NotificationHelper(context );
+        NotificationCompat.Builder nb = notificationHelper.getChannelNotification(message);
         notificationHelper.getManager().notify(1,nb.build());
     }
 }
